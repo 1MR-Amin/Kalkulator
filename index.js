f9d1115a81beaaf9cc54ec53b241da20cea0519c
1MR-Amin <84801034+1MR-Amin@users.noreply.github.com>
2021-12-16 19:55:36 +0300

    kalkulator frotent xazir

---
+++ index.js
@@ -0,0 +1,39 @@
+function forclear() {
+    document.getElementById("output").innerHTML = "0";
+}
+
+<button id="r1-1" onclick="forclear()" style="width: 138px;">
+     C
+</button>
+
+function removeZero() {
+    var value = document.getElementById("output").innerHTML;
+    if (value == "0") {
+         value = " "
+         document.getElementById("output").innerHTML = value;
+    }
+}
+
+function perc() {
+    var value = document.getElementById("output").innerHTML;
+    value = value / 100;
+    document.getElementById("output").innerHTML = value;
+}
+
+<button id="r1-3" onclick="perc()"> % </button>
+
+function fordisplay(value) {
+    removeZero()
+    document.getElementById("output").innerHTML += value;
+}
+
+<button id="r3-1" value="4" onclick="fordisplay('4') ">4</button>
+
+function solve() {
+    removeZero()
+    var equation = document.getElementById("output").innerHTML;
+    var solved = eval(equation);
+    document.getElementById('output').innerHTML = solved;
+}
+
+<button id="r5-3" value="=" onclick="solve()" >=</button>
